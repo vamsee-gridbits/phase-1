134ca9001f9657a88e8f637466fa7de7346160d4
Vamsee Chamakura <vamseems@gmail.com>
2014-06-27 17:18:01 +0530

    minute change

--- js/script.js
+++ js/script.js
@@ -4,3 +4,11 @@ $(document).ready(function() {
           top: $('header').height()
         }});
 });
+
+function height_carousel_mine () {
+  var a = document.getElementById('one_slide_carousel').clientHeight;
+  document.getElementById('two_slide_carousel').style.width=a;
+  console.log("DONE 1");
+  document.getElementById('three_slide_carousel').style.width=a;
+  console.log("DONE 2");
+}
